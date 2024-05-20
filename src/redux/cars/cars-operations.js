75e52e592f6931e5b90421b90923861d74db2e8e
Tamira2804 <tamira2804@gmail.com>
2024-05-20 14:02:03 +0300

    Update cars-operations.js

--- src/redux/cars/cars-operations.js
+++ src/redux/cars/cars-operations.js
@@ -1,32 +1,32 @@
-import axios from "axios";
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios'
+import { createAsyncThunk } from '@reduxjs/toolkit'
 
-axios.defaults.baseURL = "https://65016131736d26322f5b994c.mockapi.io";
+axios.defaults.baseURL = 'https://65722d8ad61ba6fcc0147f61.mockapi.io'
 
 export const fetchAllCars = createAsyncThunk(
-  "adverts/fetchAllCars",
+  'adverts/fetchAllCars',
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get(`/adverts`);
+      const response = await axios.get(`/adverts`)
 
-      return response.data;
+      return response.data
     } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
+      return thunkAPI.rejectWithValue(e.message)
     }
   }
-);
+)
 
 export const fetchCarsByPage = createAsyncThunk(
-  "adverts/fetchCarsByPage",
+  'adverts/fetchCarsByPage',
   async ({ page = 1, limit = 12 }, thunkAPI) => {
     try {
       const response = await axios.get(`/adverts?`, {
         params: { page, limit },
-      });
+      })
 
-      return response.data;
+      return response.data
     } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
+      return thunkAPI.rejectWithValue(e.message)
     }
   }
-);
+)
